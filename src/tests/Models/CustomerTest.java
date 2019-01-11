33abc5f07d371761c99e7d9a5334bb7247ed8956
Unknown <lorenzowijtman@hotmail.com>
2019-01-11 11:16:59 +0100

    added 1 test

--- src/tests/Models/CustomerTest.java
+++ src/tests/Models/CustomerTest.java
@@ -6,6 +6,8 @@
 package Models;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import org.junit.After;
@@ -41,18 +43,7 @@ public class CustomerTest {
         ResultSet result = instance.getCustomers();
         assertTrue(result.first());
     }
-    
-    /**
-     * Test to see if the method getCustomers returns at least 1 flight/row
-     * @throws Exception
-     */
-    @Test
-    public void testGetCustomer() throws Exception {
-        System.out.println("get customer");
-        ResultSet result = instance.getCustomer(1);
-        assertTrue(result.first());
-    }
-    
+
     /**
      * Test to see if the method getCustomers returns 0 results with an false ID
      * @throws Exception
@@ -63,13 +54,17 @@ public class CustomerTest {
         ResultSet result = instance.getCustomer(123456);
         assertFalse(result.first());
     }
-    
+
+    /**
+     * Test to see if the method saveCustomer saves a customer
+     * @throws Exception
+     */
     @Test
     public void testSaveCustomer() throws Exception {
         System.out.println("make a flight");
 
-        String name = "Costa";
-        String email = "elsasc@hva.nl";
+        String name = "Lorenzo";
+        String email = "lorenzo@hva.nl";
         String address = "Wibautstraat";
         String city = "Amsterdam";
         String zipcode = "1000AA";
@@ -84,12 +79,38 @@ public class CustomerTest {
         assertThat(expResult, equalTo(result));
     }
 
+    /**
+     * Test to see if the method setName changes the name of the current customer
+     */
     @Test
     public void NameCanBeChanged() {
         String newName = "This is a new name";
         instance.setName(newName);
 
         assertThat(newName, equalTo(instance.getName()));
+    }
+
+    /**
+     * Test to see if the method getLastID returns the last added customer
+     * @throws Exception
+     */
+    @Test
+    public void getLastIdTest() throws SQLException {
+        String name = "Lorenzo";
+        String email = "lorenzo@hva.nl";
+        String address = "Wibautstraat";
+        String city = "Amsterdam";
+        String zipcode = "1000AA";
+        String country = "NL";
+        String phoneNumber = "06123456789";
+        int firstId = Integer.parseInt(instance.getLastId());
+
+        instance.Customer(name, email, address, city, zipcode, country, phoneNumber);
+        instance.saveCustomer();
+        int secondId = Integer.parseInt(instance.getLastId());
+
+        assertTrue(firstId < secondId);
+
     }
 
 }
